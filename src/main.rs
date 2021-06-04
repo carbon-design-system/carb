a7254dbdb781ba4c0487f96dadede0baaa4053e0
Josh Black <josh@josh.black>
2021-06-04 17:43:00 -0500

    feat(cli): add initial cli scaffold (#11)

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
+use structopt::StructOpt;
 use tracing::info;
+
+#[derive(Debug, StructOpt)]
+enum Cli {
+    /// Interact with the cache in your project
+    Cache,
+
+    /// Remove any generated files or folders in your project
+    Clean,
+
+    /// Run tasks associated with workspaces in your project
+    Run {
+        /// The name of the task to run
+        task: String,
+    },
+}
 
 fn main() {
     tracing_subscriber::fmt::init();
-    info!("Hello world");
-}
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    match Cli::from_args() {
+        Cli::Cache => {
+            info!("Running the cache command");
+        }
+        Cli::Clean => {
+            info!("Running the clean command");
+        }
+        Cli::Run { task } => {
+            info!("Running the {} command", task);
+        }
+    };
 }
