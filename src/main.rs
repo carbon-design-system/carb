cd8bcb8111d70525ded51184f8e32bf718ad9f98
Josh Black <josh@josh.black>
2021-07-07 20:26:49 -0500

    feat(cli): refactor and add config module

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,80 @@
+use env_logger::{fmt::Color, Builder};
+use log::{debug, error, info, Level};
+use std::env;
+use std::process;
 use structopt::StructOpt;
-use tracing::info;
 
-#[derive(Debug, StructOpt)]
-enum Cli {
-    /// Interact with the cache in your project
-    Cache,
+mod config;
 
-    /// Remove any generated files or folders in your project
-    Clean,
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(subcommand)]
+    cmd: Option<Subcommand>,
 
-    /// Run tasks associated with workspaces in your project
-    Run {
-        /// The name of the task to run
-        task: String,
-    },
+    #[structopt(flatten)]
+    verbose: clap_verbosity_flag::Verbosity,
 }
 
-fn main() {
-    tracing_subscriber::fmt::init();
+#[derive(StructOpt)]
+enum Subcommand {
+    #[structopt(about = "initialize your project")]
+    Init,
+}
 
-    match Cli::from_args() {
-        Cli::Cache => {
-            info!("Running the cache command");
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn main() {
+    let Cli { cmd, verbose } = Cli::from_args();
+
+    init_logger(verbose.log_level());
+
+    if let Err(err) = try_main(cmd) {
+        error!("{}", err);
+        process::exit(1);
+    }
+
+    info!("Done! ✨");
+}
+
+fn try_main(cmd: Option<Subcommand>) -> Result<()> {
+    match cmd {
+        None => {
+            info!("Default command");
+            Ok(())
         }
-        Cli::Clean => {
-            info!("Running the clean command");
+
+        Some(Subcommand::Init) => {
+            info!("Creating a new project...");
+
+            let cwd = env::current_dir().expect("Expected to find current working directory");
+
+            debug!("Running from: {:?}", cwd);
+
+            Ok(())
         }
-        Cli::Run { task } => {
-            info!("Running the {} command", task);
-        }
-    };
+    }
+}
+
+fn init_logger(level: Option<Level>) {
+    if let Some(level) = level {
+        let mut builder = Builder::from_default_env();
+
+        builder
+            .filter(None, level.to_level_filter())
+            .format(move |buf, record| {
+                use std::io::Write;
+
+                let mut style = buf.style();
+                let formatted = match record.level() {
+                    Level::Trace => style.set_color(Color::Magenta).value("trace"),
+                    Level::Debug => style.set_color(Color::Blue).value("debug"),
+                    Level::Info => style.set_color(Color::Green).value("info"),
+                    Level::Warn => style.set_color(Color::Yellow).value("warn"),
+                    Level::Error => style.set_color(Color::Red).value("error"),
+                };
+
+                writeln!(buf, "{} {}", formatted, record.args())
+            })
+            .init()
+    }
 }
